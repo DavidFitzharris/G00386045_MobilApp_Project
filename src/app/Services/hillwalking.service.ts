e077260fe3e8d4dd4fb752a89e1a176f06020bfb
David Fitzharris <g00386045@gmit.ie>
2021-05-12 17:19:16 +0100

    Setup

---
+++ src/app/Services/hillwalking.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HillwalkingService {
+
+  constructor(private http:HttpClient) { }
+  
+}
